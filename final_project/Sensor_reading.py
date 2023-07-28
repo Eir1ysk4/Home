44710d0926a6ab098d78d7a38eaa1adf321bc9a6
Tanhien1 <135790327+Tanhien1@users.noreply.github.com>
2023-07-28 20:07:41 +0700

    Update Sensor_reading.py

--- final_project/Sensor_reading.py
+++ final_project/Sensor_reading.py
@@ -8,9 +8,15 @@ except:
 
 def sendCommand (cmd):
     ser.write(cmd.encode())
+
+def requestData(cmd):
+    sendCommand(cmd)
+    time.sleep(1)
+    readSerial()
     
 mess = ""
 tempData = 0 # to save sensor readings to a tempData
+
 def processData(data):
     global tempData
 
@@ -19,8 +25,7 @@ def processData(data):
     splitData = data.split(":")
     #save readings to tempData
     tempData = splitData
-    print(splitData)
-
+    
 def readSerial():
     bytesToRead = ser.inWaiting()
     if (bytesToRead > 0):
@@ -30,7 +35,6 @@ def readSerial():
             start = mess.find("!")
             end = mess.find("#")
             processData(mess[start:end + 1])
-            print(mess)
             if (end == len(mess)):
                 mess = ""
             else:
@@ -40,46 +44,14 @@ def readTemperature():
 # read temperature input via sensors
 # print out temperature in C and alert when goes over certain value
     print("reading temp")
-    for _ in range (5):
-        global tempData
-        #requesting reading of temp
-        requestData("0")
-        print (float(tempData[2]))
-        #if temp is higher than some num, turn on light 
-        if float(tempData[2]) > 29:
-            sendCommand("4")
+    global tempData
+    #requesting reading of temp
+    requestData("0")
+     #if temp is higher than some num, turn on light 
+    if float(tempData[2]) > 31:
+        sendCommand("4")
     sendCommand("5")
     print ("end of reading")
 
 
-def readHumidity():
-# read humidity input via sensors
-# print out humidity in percentage of water in surrounding environment
-# and alert(via turning on LED light) when goes over certain value
-    print("reading humidity")
-    for _ in range (5):
-        global tempData
-        #requesting reading of humidity
-        requestData("1")
-        print (float(tempData[2]))
-        #if humidity is higher than some num, turn on light 
-        if float(tempData[2]) > 45:
-            sendCommand("4")
-    sendCommand("5")
-    print ("end of reading")
-    
-
-def requestData(cmd):
-    sendCommand(cmd)
-    time.sleep(1)
-    readSerial()
-
-# sendCommand("2")
-# time.sleep(2)
-# sendCommand("3")
-# time.sleep(2)
-# sendCommand("4")
-# time.sleep(2)
-# sendCommand("5")
-# readTemperature()
 
