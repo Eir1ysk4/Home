59af8bbb84622518b9dc3e7adaf932d5738a08f0
Tanhien1 <135790327+Tanhien1@users.noreply.github.com>
2023-07-30 20:39:15 +0700

    Update final_project.py

--- final_project/final_project.py
+++ final_project/final_project.py
@@ -17,20 +17,24 @@ AIO_FEED_ID = ["bmi","blood-pressure","	burned-calories"
 AIO_USERNAME = "Eir1ysk4"
 AIO_KEY = "aio_wjot67ee6SikXLGPmBbInILhPS9C"
 
-
+# connect to server and subscribe to feeds
 def connected(client):
     print("Connected to server!!!")
     for topic in AIO_FEED_ID:
         client.subscribe(topic)
     client.subscribe("body-temperature")
 
+# relay successful subscription
 def subscribe(client , userdata , mid , granted_qos):
     print("Subscribed successfully...")
 
+# disconnect client from server
 def disconnected(client):
     print("Disconnected from server!")
     sys.exit (1)
 
+# handle communicating actions sent 
+# and check for specific feed to show special message
 def message(client , feed_id , payload):
     print(f"{feed_id} received data: {payload}")
 
@@ -38,8 +42,7 @@ def message(client , feed_id , payload):
         BMI_equation = payload
         print (BMI_equation)
 
-
-
+# initiating connection process and requirements
 client = MQTTClient(AIO_USERNAME , AIO_KEY)
 client.on_connect = connected
 client.on_disconnect = disconnected
